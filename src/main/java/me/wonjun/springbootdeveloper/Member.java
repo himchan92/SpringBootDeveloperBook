ccc2e58d323c67fa4f5a3161b0eb584327874c46
omea92 <thdjun92@naver.com>
2025-05-08 23:49:42 +0900

    init commit

---
+++ src/main/java/me/wonjun/springbootdeveloper/Member.java
@@ -0,0 +1,26 @@
+package me.wonjun.springbootdeveloper;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
